f72436be59826549568aed652ba1810b4e7605a6
adrianRacovitaJSX <aracovita@iempresa.net>
2024-06-28 01:33:04 +0200

    github link

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,12 @@ const Header = () => {
     <div className="mb-10 w-full flex items-center justify-between p-5 dark:bg-black dark:backdrop-blur-lg backdrop-blur-lg z-50 top-0 bg-opacity-50 dark:bg-opacity-50 dark:backdrop-filter sticky">
       <h1 className="text-3xl font-bold">Prediciones Eurocopa 2024</h1>
       <div className="flex gap-4">
+        <a href="https://github.com/adrianRacovitaJSX/euros24-app" target="_blank" rel="noreferrer">
         <button className="dark:bg-white bg-black text-white dark:text-black px-4 py-2 border-b dark:border-0 rounded-xl flex gap-2">
         <Github />Github
         </button>
+        </a>
+      
         <ModeToggle />
       </div>
     </div>
